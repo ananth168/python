594bb58a56c2ea492e275f89e4de1a8733014609
ananth168 <ananthramganjal2001@gmail.com>
2021-06-04 10:55:18 +0530

    button in tkinter

--- demo.py
+++ demo.py
@@ -1,15 +1,16 @@
-def search(arr, key):  #this function searches for a given key in a input array
+def search(arr, key):  # this function searches for a given key in a input array
 
     for ii in range(len(arr)):
 
         if arr[ii] == key:
-            print ("found at ", ii)
+            print("found at ", ii)
             return
 
     print("not found")
 
+
 arr = [7, 3, 5, 1, 8]
 key = (int(input("enter any number  ")))
 
-result = search(arr,key)
-print(result)
+print(search(arr, key))
+
