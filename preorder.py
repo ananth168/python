bb3f6e50296b6f1bda1f0bcda80f68e986537d30
ananth168 <65211656+ananth168@users.noreply.github.com>
2021-06-15 13:37:58 +0530

    DFS(preorder)

---
+++ preorder.py
@@ -0,0 +1,38 @@
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+class BinaryTree():
+    def __init__(self, value):
+        self.root = Node(value)
+
+
+    
+
+    def Preorder(self, start, traversal):
+        if start is None:
+            return
+
+        traversal.append(start.value)
+        self.Preorder(start.left, traversal)  
+        self.Preorder(start.right, traversal)  
+
+        return traversal
+
+
+
+
+
+tree = BinaryTree(3)
+
+tree.root.left = Node(4)
+tree.root.left.right = Node(2)
+tree.root.left.left = Node(6)
+
+tree.root.right = Node(7)
+tree.root.right.right = Node(1)
+tree.root.right.left = Node(9)
+
+print(tree.Preorder(tree.root, []))
