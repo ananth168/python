df0acf19b449cf82652c3ff0dc5f584122ce4a6a
ananth168 <65211656+ananth168@users.noreply.github.com>
2021-06-15 13:58:17 +0530

    DFS(postorder)

---
+++ postorder.py
@@ -0,0 +1,36 @@
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+class BinaryTree():
+    def __init__(self, value):
+        self.root = Node(value)
+
+
+    def Postorder(self, start, traversal):
+        if start is None:
+            return
+
+        self.Postorder(start.left, traversal)  
+        self.Postorder(start.right, traversal)  
+        traversal.append(start.value)
+
+        return traversal
+
+
+
+
+
+tree = BinaryTree(3)
+
+tree.root.left = Node(4)
+tree.root.left.right = Node(2)
+tree.root.left.left = Node(6)
+
+tree.root.right = Node(7)
+tree.root.right.right = Node(1)
+tree.root.right.left = Node(9)
+
+print(tree.Postorder(tree.root, []))
